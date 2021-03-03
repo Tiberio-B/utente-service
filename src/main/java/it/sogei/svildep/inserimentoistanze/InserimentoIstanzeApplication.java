bbff59f5750ede917943d076d47e12c5038d3f82
Tiberio <tiberio.b@hotmail.it>
2021-03-03 17:44:10 +0100

    init

---
+++ src/main/java/it/sogei/svildep/inserimentoistanze/InserimentoIstanzeApplication.java
@@ -0,0 +1,13 @@
+package it.sogei.svildep.inserimentoistanze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InserimentoIstanzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InserimentoIstanzeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/sogei/svildep/inserimentoistanze/InserimentoIstanzeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sogei.svildep.inserimentoistanze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InserimentoIstanzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
